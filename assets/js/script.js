01921d41432a2e6bf473526d7295777b25cc8e99
Felipe Castillo <felipe.rco@gmail.com>
2024-07-03 01:39:38 -0400

    cambiando formato

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,93 +1,69 @@
-import propiedades_venta from "./propiedades_venta.js";
-import propiedades_alquiler from "./propiedades_alquiler.js";
+const tareas = [
+    { id: 100, nombre: "Asistir a clases", realizado: false },
+    { id: 101, nombre: "Hacer las tareas", realizado: false },
+    { id: 102, nombre: "Realizar desafio", realizado: false },
+  ];
 
-const venta = document.querySelector("#propiedades-venta");
-const alquiler = document.querySelector("#propiedades-alquiler");
+  const btn = document.querySelector("#agregar");
+  const lista = document.querySelector("#lista");
 
-if(venta !== null ){
-    let html = ''
-    for(let propiedad of propiedades_venta){
-    html += `
-    <div class="col-md-4 mb-4">
-    <div class="card">
-    <img src="${propiedad.src}" class="card-img-top" alt="Imagen del departamento"/>
-    <div class="card-body">
-    <h5 class="card-title">${propiedad.nombre}</h5>
-    <p class="card-text">${propiedad.descripcion}</p>
-    <p><i class="fas fa-map-marker-alt"></i> ${propiedad.ubicacion}</p>
-    <p><i class="fas fa-bed"></i> ${propiedad.habitaciones} Habitaciones |<i class="fas fa-bath"></i> ${propiedad.banos} Baños</p>
-    <p><i class="fas fa-dollar-sign"></i> ${propiedad.costo}</p>
-    <p class="${propiedad.smoke ? 'text-success' : 'text-danger'}"><i class="fas ${propiedad.smoke ? 'fa-smoking' : 'fa-smoking-ban'} "></i> ${propiedad.smoke ? 'Permitido fumar' : 'No se permite fumar'} </p>
-    <p class="${propiedad.pets ? 'text-success' : 'text-danger'}"><i class="fas ${propiedad.pets ? 'fa-paw' : 'fa-ban'} "></i> ${propiedad.pets ? 'Mascotas permitidas' : 'No se permiten mascotas'} </p>
-    </div>
-    </div>
-    </div>
-    `;
-    }
-    venta.innerHTML = html
-}
-if(alquiler !== null){
-    let html = ''
-    for(let propiedad of propiedades_alquiler){
-    html += `
-    <div class="col-md-4 mb-4">
-    <div class="card">
-    <img src="${propiedad.src}" class="card-img-top" alt="Imagen del departamento"/>
-    <div class="card-body">
-    <h5 class="card-title">${propiedad.nombre}</h5>
-    <p class="card-text">${propiedad.descripcion}</p>
-    <p><i class="fas fa-map-marker-alt"></i> ${propiedad.ubicacion}</p>
-    <p><i class="fas fa-bed"></i> ${propiedad.habitaciones} Habitaciones |<i class="fas fa-bath"></i> ${propiedad.banos} Baños</p>
-    <p><i class="fas fa-dollar-sign"></i> ${propiedad.costo}</p>
-    <p class="${propiedad.smoke ? 'text-success' : 'text-danger'}"><i class="fas ${propiedad.smoke ? 'fa-smoking' : 'fa-smoking-ban'} "></i> ${propiedad.smoke ? 'Permitido fumar' : 'No se permite fumar'} </p>
-    <p class="${propiedad.pets ? 'text-success' : 'text-danger'}"><i class="fas ${propiedad.pets ? 'fa-paw' : 'fa-ban'} "></i> ${propiedad.pets ? 'Mascotas permitidas' : 'No se permiten mascotas'} </p>
-    </div>
-    </div>
-    </div>
-    `;
-    }
-    alquiler.innerHTML = html
-}
-if(venta !== null && alquiler !== null){
-    let htmlventa = ''
-    for(let propiedad of propiedades_venta.slice(0, 3)){
-    htmlventa += `
-    <div class="col-md-4 mb-4">
-    <div class="card">
-    <img src="${propiedad.src}" class="card-img-top" alt="Imagen del departamento"/>
-    <div class="card-body">
-    <h5 class="card-title">${propiedad.nombre}</h5>
-    <p class="card-text">${propiedad.descripcion}</p>
-    <p><i class="fas fa-map-marker-alt"></i> ${propiedad.ubicacion}</p>
-    <p><i class="fas fa-bed"></i> ${propiedad.habitaciones} Habitaciones |<i class="fas fa-bath"></i> ${propiedad.banos} Baños</p>
-    <p><i class="fas fa-dollar-sign"></i> ${propiedad.costo}</p>
-    <p class="${propiedad.smoke ? 'text-success' : 'text-danger'}"><i class="fas ${propiedad.smoke ? 'fa-smoking' : 'fa-smoking-ban'} "></i> ${propiedad.smoke ? 'Permitido fumar' : 'No se permite fumar'} </p>
-    <p class="${propiedad.pets ? 'text-success' : 'text-danger'}"><i class="fas ${propiedad.pets ? 'fa-paw' : 'fa-ban'} "></i> ${propiedad.pets ? 'Mascotas permitidas' : 'No se permiten mascotas'} </p>
-    </div>
-    </div>
-    </div>
-    `;
-       }
-    venta.innerHTML = htmlventa 
-    
-    let htmlAlquiler = ''
-    for(let propiedad of propiedades_alquiler.slice(0, 3)){
-    htmlAlquiler += `
-    <div class="col-md-4 mb-4">
-    <div class="card">
-    <img src="${propiedad.src}" class="card-img-top" alt="Imagen del departamento"/>
-    <div class="card-body">
-    <h5 class="card-title">${propiedad.nombre}</h5>
-    <p class="card-text">${propiedad.descripcion}</p>
-    <p><i class="fas fa-map-marker-alt"></i> ${propiedad.ubicacion}</p>
-    <p><i class="fas fa-bed"></i> ${propiedad.habitaciones} Habitaciones |<i class="fas fa-bath"></i> ${propiedad.banos} Baños</p>
-    <p><i class="fas fa-dollar-sign"></i> ${propiedad.costo}</p>
-    <p class="${propiedad.smoke ? 'text-success' : 'text-danger'}"><i class="fas ${propiedad.smoke ? 'fa-smoking' : 'fa-smoking-ban'} "></i> ${propiedad.smoke ? 'Permitido fumar' : 'No se permite fumar'} </p>
-    <p class="${propiedad.pets ? 'text-success' : 'text-danger'}"><i class="fas ${propiedad.pets ? 'fa-paw' : 'fa-ban'} "></i> ${propiedad.pets ? 'Mascotas permitidas' : 'No se permiten mascotas'} </p>
-    </div>
-    </div>
-    </div>
-    `;
-   }
-alquiler.innerHTML = htmlAlquiler
-}
+  const actualizar = (id) => {
+    let tareaIndex = tareas.findIndex(
+      (tarea) => tarea.id === id
+    );
+    console.log(tareaIndex);
+    tareas[tareaIndex].realizado = !tareas[tareaIndex].realizado;
+    let tareasRealizados = tareas.filter(
+      (tarea) => tarea.realizado == true
+    );
+    console.log(tareasRealizados);
+    document.querySelector(
+      "#realizadas"
+    ).innerHTML = `Realizadas: <span class="fw-bold">${tareasRealizados.length}</span>`;
+  };
+
+  const eliminar = (id) => {
+    let tareaIndex = tareas.findIndex(
+      (tarea) => tarea.id === id
+    );
+    tareas.splice(tareaIndex, 1);
+    tarea_actual();
+  };
+
+  const tarea_actual = () => {
+    let html = ``;
+    tareas.forEach((tarea) => {
+      console.log(tarea.realizado);
+      html += `<div class="row">
+        <div class="col-2">${tarea.id}</div>
+        <div class="col-6">${tarea.nombre}</div>  
+      <div class="col-2"><input type='checkbox' onclick='actualizar(${
+        tarea.id
+      })' ${tarea.realizado === true ? "checked" : null}></div>
+      <div class="col-2"><button class="mb-1 btn btn-danger" onclick='eliminar(${
+        tarea.id
+      })'><i class="fa-solid fa-trash"></i></button></div> 
+      </div>
+      `;
+    });
+    document.querySelector(
+      "#cantidad"
+    ).innerHTML = `Total: <span class="fw-bold">${tareas.length}</span>`;
+    lista.innerHTML = html;
+  };
+
+  btn.addEventListener("click", () => {
+    let value = document.querySelector("input").value;
+    let num1 = 100;
+    let num2 = 999;
+    let numeroAleatorio = Math.floor(Math.random() * (num2 - num1 + 1)) + num1;     //para que el numero generado sea un numero entero
+    let nueva_actividad = {
+        id: numeroAleatorio,
+      nombre: value,
+      realizado: false,
+    };
+    tareas.push(nueva_actividad);
+    tarea_actual();
+  });
+
+  tarea_actual();
